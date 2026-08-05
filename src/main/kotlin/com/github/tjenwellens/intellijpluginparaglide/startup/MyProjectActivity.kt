280e6e9beb57309ca6993771e90b23ba887d68eb
GitHub Action <action@github.com>
2026-08-05 05:25:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/tjenwellens/intellijpluginparaglide/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/tjenwellens/intellijpluginparaglide/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.tjenwellens.intellijpluginparaglide.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
